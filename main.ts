f689caf2452f4cdb45e6964ba6452fb1db695a65
Chase May <hello@clm.dev>
2024-12-23 15:31:52 -0800

    Slightly refactor

--- main.ts
+++ main.ts
@@ -6,58 +6,54 @@ const response = await fetch(url);
 const html = await response.text();
 const $ = load(html);
 
-const entries = $(".list-menu-item")
-  .map((_, el) => {
-    const [tapNumber, brewery] = $(el)
-      .find(".list-menu-item__head span:nth-of-type(1)")
+const taplist = Array.from($(".list-menu-item")).map((el) => {
+  const [tapNumber, brewery] = $(el)
+    .find(".list-menu-item__head span:nth-of-type(1)")
+    .text()
+    .split(".")
+    .map((s) => s.trim());
+
+  const name = $(el)
+    .find(".list-menu-item__head span:nth-of-type(2)")
+    .text()
+    .trim();
+
+  const location = $(el)
+    .find(".list-menu-item__head span:nth-of-type(3)")
+    .text()
+    .trim();
+
+  const [description, abvText] = $(el)
+    .find(".list-menu-item__body")
+    .text()
+    .split("|")
+    .map((s) => s.trim());
+
+  const abv = parseFloat(abvText.replace(/[^0-9.]/g, ""));
+
+  const pricesTable = $(el).find(".list-menu-item__table tbody tr");
+  const prices = Array.from(pricesTable).map((row) => ({
+    size: $(row).find("td").eq(0).text().trim(),
+    price: $(row).find("td").eq(1).text().trim(),
+  }));
+
+  const percentRemaining = parseFloat(
+    $(el)
+      .find(".list-menu-item__progress strong")
       .text()
-      .split(".")
-      .map((s) => s.trim());
+      .replace(/[^0-9.]/g, "")
+  );
 
-    const name = $(el)
-      .find(".list-menu-item__head span:nth-of-type(2)")
-      .text()
-      .trim();
+  return {
+    tapNumber,
+    brewery,
+    name,
+    location,
+    description,
+    abv,
+    prices,
+    percentRemaining,
+  };
+});
 
-    const location = $(el)
-      .find(".list-menu-item__head span:nth-of-type(3)")
-      .text()
-      .trim();
-
-    const [description, abvText] = $(el)
-      .find(".list-menu-item__body")
-      .text()
-      .split("|")
-      .map((s) => s.trim());
-
-    const abv = parseFloat(abvText.replace(/[^0-9.]/g, ""));
-
-    const prices = $(el)
-      .find(".list-menu-item__table tbody tr")
-      .map((_, row) => ({
-        size: $(row).find("td").eq(0).text().trim(),
-        price: $(row).find("td").eq(1).text().trim(),
-      }))
-      .get();
-
-    const percentRemaining = parseFloat(
-      $(el)
-        .find(".list-menu-item__progress strong")
-        .text()
-        .replace(/[^0-9.]/g, "")
-    );
-
-    return {
-      tapNumber,
-      brewery,
-      name,
-      location,
-      description,
-      abv,
-      prices,
-      percentRemaining,
-    };
-  })
-  .get();
-
-console.log(JSON.stringify(entries, null, 2));
+console.log(JSON.stringify(taplist, null, 2));
